b2308efe67254b7fcf8231677deb5be088b20ed2
gabrizl <gsilva.pb@gmail.com>
2022-02-28 08:53:11 -0300

    fix group information

--- src/Console/Console.java
+++ src/Console/Console.java
@@ -1043,9 +1043,9 @@ public class Console {
                 System.out.printf("Gabriel Alves da Costa e Silva - gabriel.costa@aluno.uepb.edu.br\n");
                 System.out.printf("Github -> gabrizl\n\n");
                 System.out.printf("Jose Axaiel - jose.queiroz@aluno.uepb.edu.br\n");
-                System.out.printf("Github -> \n\n");
+                System.out.printf("Github -> Axaiel \n\n");
                 System.out.printf("Natalia Maite- natalia.guimaraes@aluno.uepb.edu.br\n");
-                System.out.printf("Github -> \n\n");
+                System.out.printf("Github -> Nataliamgs\n\n");
                 promptEnterKey();
                 clearConsole();
         }
